27ac3c5d264740341071a6238de6817b6c117916
Catherine Liu <catherineliu0@gmail.com>
2016-01-14 00:06:56 -0500

    updated login/logout routes... may or may not work

--- routes/indexController.js
+++ routes/indexController.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var User = require("../models/user");
+var Account = require("../models/account");
 var handleError = require('./utils.js').handleError;
 
 var google = require("googleapis");
@@ -28,30 +28,17 @@ router.get("/", function(req, res) {
     res.render('map', {
         userId: req.session.user._id,
         userEmail: req.session.user.email,
-        firstName: req.session.user.firstName,
-        lastName: req.session.user.lastName,
-        storyId: req.session.story._id
+        firstName: req.session.user.name.first,
+        lastName: req.session.user.name.last
     });
 });
 
-
-// Runs testing suite
-//  GET /test
-router.get('/test' , function(req, res) {
-    User.findOne({email:"mappyteam@gmail.com"}, function(err, user) {
-        if (err) handleError(res, 500, err);
-        req.session.user = user;
-        req.session.save();
-        var userId = user._id;
-        res.render('tests', {userId:userId});
-    });
-});
 
 // POST /login
 // Request body: { email: String, password: String }
 // Attempts to log in with the given credentials. Responds with { success: true } if successful or { error: String } otherwise.
 router.post('/login', function(req, res) {
-    User.findOne({ email: req.body.email, password: req.body.password})
+    Account.findOne({ email: req.body.email, password: req.body.password})
     .select("email firstName lastName isAdmin")
     .exec(function(err, user) {
         if(err) handleError(res, 500, err.message);
@@ -67,49 +54,55 @@ router.post('/login', function(req, res) {
 
 // GET /login/google
 // Redirects to Google for authentication
-router.get('/login/google', function(req, res){
-    // generate a url that asks permissions for Google Calendar and Email address scopes
-    var scopes = ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/plus.me'];
-    var url = oauth2Client.generateAuthUrl({
-        access_type : 'offline', // 'online' (default) or 'offline' (gets refresh_token)
-        scope       : scopes // If you only need one scope you can pass it as string
-    });
+// router.get('/login/google', function(req, res){
+//     // generate a url that asks permissions for Google Calendar and Email address scopes
+//     var scopes = ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/plus.me'];
+//     var url = oauth2Client.generateAuthUrl({
+//         access_type : 'offline', // 'online' (default) or 'offline' (gets refresh_token)
+//         scope       : scopes // If you only need one scope you can pass it as string
+//     });
 
-    res.redirect(url);
-});
+//     res.redirect(url);
+// });
 
 // Google will redirect to this link upon oauth acceptance
-router.get('/oauthcallback', function(req, res){
-    var code = req.query.code;
-    oauth2Client.getToken(code, function(err, tokens) {
+// router.get('/oauthcallback', function(req, res){
+//     var code = req.query.code;
+//     oauth2Client.getToken(code, function(err, tokens) {
 
-        // Now tokens contains an access_token and an optional refresh_token. Save them.
-        if (err) return handleError(res, 500, err);
-        oauth2Client.setCredentials(tokens);
+//         // Now tokens contains an access_token and an optional refresh_token. Save them.
+//         if (err) return handleError(res, 500, err);
+//         oauth2Client.setCredentials(tokens);
 
-        // user is now logged in. Using token to get personal info
-        plus.people.get({userId:'me', auth:oauth2Client}, function(err, googlePlusInfo) {
-            if (err) return handleError(res, 500, err);
-            savePersonalInfo(googlePlusInfo, req, res);
-        });
+//         // user is now logged in. Using token to get personal info
+//         plus.people.get({userId:'me', auth:oauth2Client}, function(err, googlePlusInfo) {
+//             if (err) return handleError(res, 500, err);
+//             savePersonalInfo(googlePlusInfo, req, res);
+//         });
         
-    });
-});
+//     });
+// });
 
 // POST /signup
-// Request body: { email: String, password: String, firstName: String, lastName: String }
+// Request body: { email: String, password: String, firstName: String, lastName: String, and all optional fields }
 // Attempts to create an account with the given credentials. Responds with { success: true } if successful or { error: String } otherwise.
 router.post('/signup', function(req,res) {
-    User.findOne({ email: req.body.email }, function(err, user) {
+    Account.findOne({ email: req.body.email }, function(err, user) {
         if(err) handleError(res, 500, err.message);
         else if(user) {
             res.json({ error: "Error: An account already exists with this email address" });
         } else {
             var newUser = new User({
-                email: req.body.email,
-                password: req.body.password,
-                firstName: req.body.firstName,
-                lastName: req.body.lastName
+                email : req.body.email,
+                password : req.body.password,
+                name : req.body.name,
+                location : req.body.location,
+                dob : req.body.dob,
+                image : req.body.image,
+                impairment : req.body.impairment,
+                sex : req.body.sex,
+                hobbies : req.body.hobbies,
+                description : req.body.description
             });
             newUser.save(function(err, user) {
                 if (err) return handleError(res, 500, err.message);
@@ -133,42 +126,42 @@ router.post('/logout', function(req,res){
 
 
 // parses and saves the google plus info into the Users model
-var savePersonalInfo = function(googlePlusInfo, req, res) {
-    var firstName = googlePlusInfo.name.givenName;
-    var lastName = googlePlusInfo.name.familyName;
-    var email = null;
+// var savePersonalInfo = function(googlePlusInfo, req, res) {
+//     var firstName = googlePlusInfo.name.givenName;
+//     var lastName = googlePlusInfo.name.familyName;
+//     var email = null;
 
-    // finds the email associated with the account
-    for (var i = 0; i < googlePlusInfo.emails.length; i++) {
-        var emailObject = googlePlusInfo.emails[i];
-        if (emailObject.type == 'account') {
-            email = emailObject.value;
-        }
-    }
+//     // finds the email associated with the account
+//     for (var i = 0; i < googlePlusInfo.emails.length; i++) {
+//         var emailObject = googlePlusInfo.emails[i];
+//         if (emailObject.type == 'account') {
+//             email = emailObject.value;
+//         }
+//     }
 
-    // try to find the user
-    User.findOne({ email:email }, function (err, user) {
-        if (err) return handleError(res, 500, err);
+//     // try to find the user
+//     Account.findOne({ email:email }, function (err, user) {
+//         if (err) return handleError(res, 500, err);
 
-        // create a new user and save the fact that they're new to the session
-        if (user == undefined) {
-            var newUser = new User({ 'firstName': firstName, 'lastName': lastName, email: email });
+//         // create a new user and save the fact that they're new to the session
+//         if (user == undefined) {
+//             var newUser = new User({ 'firstName': firstName, 'lastName': lastName, email: email });
         
-            newUser.save(function (err, user) {
-                if (err) return handleError(res, 500, err);
-                userObject = user;
-                //req.session.userIsNew = true;
-                req.session.user = user;
-                req.session.save();
-                res.redirect('/');
-            });
+//             newUser.save(function (err, user) {
+//                 if (err) return handleError(res, 500, err);
+//                 userObject = user;
+//                 //req.session.userIsNew = true;
+//                 req.session.user = user;
+//                 req.session.save();
+//                 res.redirect('/');
+//             });
 
-        } else {
-            req.session.user = user;
-            req.session.save();
-            res.redirect('/');
-        }
-    });
-}
+//         } else {
+//             req.session.user = user;
+//             req.session.save();
+//             res.redirect('/');
+//         }
+//     });
+// }
 
 module.exports = router;
