ccbd95e4739841ee1c6a5951bb5dbc3171c06492
Jennifer Tylock <jtylock@mit.edu>
2016-01-18 12:51:14 -0500

    Wrote routes to change account, and moderators to see and change pending events, resources, and accounts.

--- routes/accountController.js
+++ routes/accountController.js
@@ -23,7 +23,49 @@ router.get("/:id", function(req, res) {
     });
 });
 
-//PUT change user info
+// PUT /accounts/<id>
+// Request body: { email: String, password: String, firstName: String, lastName: String, and all optional fields }
+// Updates account information, so long as it belongs to the currently logged-in user.
+// Cannot change email or name
+router.put("/:id", function(req, res) {
+    Account.findOne({ _id: req.params.id}, function(err, user) {
+        if(err) {
+            res.json({ error: err.message });
+        } else if(user) {
+            if(req.body.password) {
+                // store a hashed password
+                user.password = crypto.createHash('md5')
+                    .update(req.body.password)
+                    .digest('hex');
+            }
+
+            if (req.body.displayName) user.displayName = req.body.displayName;
+
+            if(req.body.country) user.country = req.body.country;
+            if(req.body.state) user.state = req.body.state;
+            if(req.body.town) user.town = req.body.town;
+            if(req.body.displayLocation || !req.body.displayLocation) user.displayLocation = req.body.displayLocation;
+            if(req.body.dob) user.dob = req.body.dob;
+            if(req.body.displayDOB || !req.body.displayDOB) user.displayDOB = req.body.displayDOB;
+
+            if(req.body.image) user.image = req.body.image;
+            if(req.body.impairment) user.impairment = req.body.impairment;
+            if(req.body.sex) user.sex = req.body.sex;
+            if(req.body.hobbies) user.hobbies = req.body.hobbies;
+            if(req.body.description) user.description = req.body.description;
+
+            user.save(function(err) {
+                if(err) {
+                    res.json({ error: err.message });
+                } else {
+                    res.json({ success: true });
+                }
+            });
+        } else {
+            res.json({ error: "Error: You do not have permission to edit this account" });
+        }
+    });
+});
 
 /*
 GET /accounts/<id>/events
@@ -67,4 +109,146 @@ router.get('/:id/resources', function(req,res) {
         handleError(res, 500, "Error: You do not have the permission to view these resources");
     }
 });
+
+/*
+GET /accounts/moderator/pendingresources
+Gets all pending resources
+*/
+router.get('/moderator/pendingresources', function(req,res){
+    if(req.session.user.isAdmin) {
+        Resource.find({ approval : false })
+        .exec(function(err, resources) {
+            if(err) {
+                handleError(res, 500, err.message);
+            } else if(resources) {
+                res.json(resources);
+            } else {
+                res.json({ error: "Error: Unable to get pending resources" });
+            }
+        });
+    } else {
+        handleError(res, 500, "Error: You do not have the permission to view these resources");
+    }
+});
+
+/*
+GET /accounts/moderator/pendingevents
+Gets all pending events
+*/
+router.get('/moderator/pendingevents', function(req,res){
+    if(req.session.user.isAdmin) {
+        Event.find({ approval : false })
+        .exec(function(err, events) {
+            if(err) {
+                handleError(res, 500, err.message);
+            } else if(events) {
+                res.json(events);
+            } else {
+                res.json({ error: "Error: Unable to get pending events" });
+            }
+        });
+    } else {
+        handleError(res, 500, "Error: You do not have the permission to view these events");
+    }
+});
+
+/*
+GET /accounts/moderator/pendingaccounts
+Gets all pending accounts
+*/
+router.get('/moderator/pendingaccounts', function(req,res){
+    if(req.session.user.isAdmin) {
+        Account.find({ approval : false })
+        .exec(function(err, users) {
+            if(err) {
+                handleError(res, 500, err.message);
+            } else if(users) {
+                res.json(users);
+            } else {
+                res.json({ error: "Error: Unable to get pending users" });
+            }
+        });
+    } else {
+        handleError(res, 500, "Error: You do not have the permission to view these users");
+    }
+});
+
+// PUT /accounts/moderator/pendingresources
+// Request body: {TODO}
+// Updates resource information and/or changes approval status.
+router.put("/moderator/pendingresources/:id", function(req, res) {
+    Resource.findOne({ _id: req.params.id}, function(err, resource) {
+        if(err) {
+            res.json({ error: err.message });
+        } else if(resource) {
+            if (req.body.name) resource.name = req.body.name;
+            if (req.body.location) resource.location = req.body.location;
+            if (req.body.description) resource.description = req.body.description;
+            if (req.body.image) resource.image = req.body.image;
+            if (req.body.approval) resource.approval = req.body.approval;
+
+            resource.save(function(err) {
+                if(err) {
+                    res.json({ error: err.message });
+                } else {
+                    res.json({ success: true });
+                }
+            });
+        } else {
+            res.json({ error: "Error: You do not have permission to edit this resource" });
+        }
+    });
+});
+
+// PUT /accounts/moderator/pendingevents
+// Request body: {TODO}
+// Updates event information and/or changes approval status.
+router.put("/moderator/pendingevents/:id", function(req, res) {
+    Resource.findOne({ _id: req.params.id}, function(err, event) {
+        if(err) {
+            res.json({ error: err.message });
+        } else if(event) {
+            if (req.body.name) event.name = req.body.name;
+            if (req.body.location) event.location = req.body.location;
+            if (req.body.dateofevent) event.dateofevent = req.body.dateofevent;
+            if (req.body.description) event.description = req.body.description;
+            if (req.body.image) event.image = req.body.image;
+            if (req.body.approval) event.approval = req.body.approval;
+
+            event.save(function(err) {
+                if(err) {
+                    res.json({ error: err.message });
+                } else {
+                    res.json({ success: true });
+                }
+            });
+        } else {
+            res.json({ error: "Error: You do not have permission to edit this event" });
+        }
+    });
+});
+
+// PUT /accounts/moderator/pendingaccounts
+// Request body: {TODO}
+// Change approval status of an account
+router.put("/moderator/pendingaccounts/:id", function(req, res) {
+    Resource.findOne({ _id: req.params.id}, function(err, account) {
+        if(err) {
+            res.json({ error: err.message });
+        } else if(account) {
+            if (req.body.approval) resource.approval = req.body.approval;
+
+            account.save(function(err) {
+                if(err) {
+                    res.json({ error: err.message });
+                } else {
+                    res.json({ success: true });
+                }
+            });
+        } else {
+            res.json({ error: "Error: You do not have permission to edit this account" });
+        }
+    });
+});
+
 module.exports = router;
